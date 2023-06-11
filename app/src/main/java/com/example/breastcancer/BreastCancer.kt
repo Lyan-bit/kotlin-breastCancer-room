f8d4c636acf3bba29f0e7b0ed3da1e4599d4de21
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 16:49:49 +0100

    Update BreastCancer.kt

--- app/src/main/java/com/example/breastcancer/BreastCancer.kt
+++ app/src/main/java/com/example/breastcancer/BreastCancer.kt
@@ -5,32 +5,32 @@ import java.util.HashMap
 class BreastCancer {
 
     init {
-        BreastCancerAllInstances.add(this)
+        breastCancerAllInstances.add(this)
     }
 
     companion object {
-        var BreastCancerAllInstances = ArrayList<BreastCancer>()
+        var breastCancerAllInstances = ArrayList<BreastCancer>()
         fun createBreastCancer(): BreastCancer {
             return BreastCancer()
         }
         
-        var BreastCancerIndex: HashMap<String, BreastCancer> = HashMap<String, BreastCancer>()
+        var breastCancerIndex: HashMap<String, BreastCancer> = HashMap<String, BreastCancer>()
         
         fun createByPKBreastCancer(idx: String): BreastCancer {
-            var result: BreastCancer? = BreastCancerIndex[idx]
+            var result: BreastCancer? = breastCancerIndex[idx]
             if (result != null) { return result }
                   result = BreastCancer()
-                  BreastCancerIndex.put(idx,result)
+                  breastCancerIndex.put(idx,result)
                   result.id = idx
                   return result
         }
         
 		fun killBreastCancer(idx: String?) {
-            val rem = BreastCancerIndex[idx] ?: return
+            val rem = breastCancerIndex[idx] ?: return
             val remd = ArrayList<BreastCancer>()
             remd.add(rem)
-            BreastCancerIndex.remove(idx)
-            BreastCancerAllInstances.removeAll(remd)
+            breastCancerIndex.remove(idx)
+            breastCancerAllInstances.removeAll(remd)
         }        
     }
 
