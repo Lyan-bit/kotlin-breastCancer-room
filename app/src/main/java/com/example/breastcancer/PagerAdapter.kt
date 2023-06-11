67878ea41acabb7886f3dbab147b1e473503a324
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 16:54:15 +0100

    Update PagerAdapter.kt

--- app/src/main/java/com/example/breastcancer/PagerAdapter.kt
+++ app/src/main/java/com/example/breastcancer/PagerAdapter.kt
@@ -8,17 +8,19 @@ import androidx.fragment.app.FragmentPagerAdapter
 class PagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     companion object {
-        private val TABTITLES = arrayOf("+BreastCancer", "ListBreastCancer", "ClassifyBreastCancer")
+        private val TABTITLES = arrayOf("+BreastCancer", "BreastCancers", "ClassifyBreastCancer")
     }
 
     override fun getItem(position: Int): Fragment {
         // instantiate a fragment for the page.
             return when (position) {
-                0 -> { 
+                 0 -> { 
                     CreateBreastCancerFragment.newInstance(mContext) 
-                }            1 -> { 
+                }
+                 1 -> { 
                     ListBreastCancerFragment.newInstance(mContext) 
-                }            2 -> { 
+                }
+                 2 -> { 
                     ClassifyBreastCancerFragment.newInstance(mContext) 
                 }
                 else -> CreateBreastCancerFragment.newInstance(mContext) 
