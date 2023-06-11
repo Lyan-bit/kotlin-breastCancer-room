583394bd62ce4d5aaa3b7ce844ce322a9acf40c5
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 16:51:36 +0100

    Update BreastCancerRecyclerViewAdapter.kt

--- app/src/main/java/com/example/breastcancer/BreastCancerRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/breastcancer/BreastCancerRecyclerViewAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.breastcancer
 
-import android.annotation.SuppressLint
+import android.annotation.SuppressLint 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
