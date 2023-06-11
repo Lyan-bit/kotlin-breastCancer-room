235f47a03395be9e46a4d3e21d1f4d0e21ca169e
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-06-11 16:53:15 +0100

    Update ListBreastCancerFragment.kt

--- app/src/main/java/com/example/breastcancer/ListBreastCancerFragment.kt
+++ app/src/main/java/com/example/breastcancer/ListBreastCancerFragment.kt
@@ -1,6 +1,6 @@
 package com.example.breastcancer
 	
-import android.content.Context
+import android.content.Context 
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
